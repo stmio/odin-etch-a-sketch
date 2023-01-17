e94161ea01fcefac709afdf4d2223a1fd5f63e9c
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-01-17 12:07:00 +0000

    Allow grid size to be changed

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function createGrid(size) {
   container = document.getElementById("container");
+  container.replaceChildren();
+
+  if (!size) {
+    let newSize = prompt("Enter new grid size (max 100)");
+    if (newSize > 100) newSize = 100;
+    size = parseInt(newSize);
+  }
 
   for (let y = 0; y < size; y++) {
     const row = document.createElement("div");
@@ -23,5 +30,8 @@ function createGrid(size) {
 function changeColour(square) {
   square.style.backgroundColor = "black";
 }
+
+const gridSizeButton = document.querySelector(".grid-size");
+gridSizeButton.addEventListener("click", () => createGrid());
 
 createGrid(16);
